d74064a69808d3be3c451acd2d834a161184fa10
calvinH567 <76797277+calvinH567@users.noreply.github.com>
2022-03-04 17:34:57 -0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,25 @@
+def countWordsFromFile():
+    fileName =  input("Enter the file name:- ")
+
+    numberOfWords = 0
+
+    file =  open(fileName, 'r')
+    for line in file:
+        words = line.split()
+        numberOfWords = numberOfWords + len(words)
+    print("Number of words:")
+    print(numberOfWords)
+
+
+countWordsFromFile()
+# def func():
+#     fileName =input("Enter File Name: ")
+#     numberOfWords=0
+#     file=open(fileName,'r')
+#     for line in file:
+#         words=line.split()
+#         numberOfWords=numberOfWords+len(words)
+#     print(numberOfWords)
+
+# func()
+# #calvin_huynh
